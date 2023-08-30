6c76f9e9f80f19d399761827bdbc4e20d1de1d86
EuDs63 <erica23187@gmail.com>
2023-08-30 09:20:07 +0800

    route修改

--- src/routes.jsx
+++ src/routes.jsx
@@ -27,7 +27,7 @@ export const routes = [
       {
         icon: <HomeIcon {...icon} />,
         name: "dashboard",
-        path: "/home",
+        path: "/dashboard",
         element: <DashBoard />,
       },
       {
