5c8fde33f420f8be3f0b9b2ca9977a6564b2e925
EuDs63 <erica23187@gmail.com>
2023-09-06 10:01:58 +0800

    修改设置页

--- src/pages/user/setting.jsx
+++ src/pages/user/setting.jsx
@@ -69,7 +69,7 @@ import {
                 </Tabs>
               </div>
             </div>
-            <div className="gird-cols-1 mb-12 grid gap-12 px-4 lg:grid-cols-2 xl:grid-cols-3">
+            <div className="gird-cols-1 mb-12 grid gap-12 px-4 lg:grid-cols-2 xl:grid-cols-2">
               <div>
                 <Typography variant="h6" color="blue-gray" className="mb-3">
                   Platform Settings
@@ -119,97 +119,6 @@ import {
                   </Tooltip>
                 }
               />
-              <div>
-                <Typography variant="h6" color="blue-gray" className="mb-3">
-                  Platform Settings
-                </Typography>
-                <ul className="flex flex-col gap-6">
-                  {conversationsData.map((props) => (
-                    <MessageCard
-                      key={props.name}
-                      {...props}
-                      action={
-                        <Button variant="text" size="sm">
-                          reply
-                        </Button>
-                      }
-                    />
-                  ))}
-                </ul>
-              </div>
-            </div>
-            <div className="px-4 pb-4">
-              <Typography variant="h6" color="blue-gray" className="mb-2">
-                Projects
-              </Typography>
-              <Typography
-                variant="small"
-                className="font-normal text-blue-gray-500"
-              >
-                Architects design houses
-              </Typography>
-              <div className="mt-6 grid grid-cols-1 gap-12 md:grid-cols-2 xl:grid-cols-4">
-                {projectsData.map(
-                  ({ img, title, description, tag, route, members }) => (
-                    <Card key={title} color="transparent" shadow={false}>
-                      <CardHeader
-                        floated={false}
-                        color="gray"
-                        className="mx-0 mt-0 mb-4 h-64 xl:h-40"
-                      >
-                        <img
-                          src={img}
-                          alt={title}
-                          className="h-full w-full object-cover"
-                        />
-                      </CardHeader>
-                      <CardBody className="py-0 px-1">
-                        <Typography
-                          variant="small"
-                          className="font-normal text-blue-gray-500"
-                        >
-                          {tag}
-                        </Typography>
-                        <Typography
-                          variant="h5"
-                          color="blue-gray"
-                          className="mt-1 mb-2"
-                        >
-                          {title}
-                        </Typography>
-                        <Typography
-                          variant="small"
-                          className="font-normal text-blue-gray-500"
-                        >
-                          {description}
-                        </Typography>
-                      </CardBody>
-                      <CardFooter className="mt-6 flex items-center justify-between py-0 px-1">
-                        <Link to={route}>
-                          <Button variant="outlined" size="sm">
-                            view project
-                          </Button>
-                        </Link>
-                        <div>
-                          {members.map(({ img, name }, key) => (
-                            <Tooltip key={name} content={name}>
-                              <Avatar
-                                src={img}
-                                alt={name}
-                                size="xs"
-                                variant="circular"
-                                className={`cursor-pointer border-2 border-white ${
-                                  key === 0 ? "" : "-ml-2.5"
-                                }`}
-                              />
-                            </Tooltip>
-                          ))}
-                        </div>
-                      </CardFooter>
-                    </Card>
-                  )
-                )}
-              </div>
             </div>
           </CardBody>
         </Card>
