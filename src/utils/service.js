a57c1abe1e14885e3bfb252c7eadc4f77ecdb7cb
EuDs63 <erica23187@gmail.com>
2023-09-06 18:39:20 +0800

    修改baseURL位置

--- src/utils/service.js
+++ src/utils/service.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 let service = axios.create({
-  // baseURL: 'http://127.0.0.1:5000',
-  baseURL: "http://10.208.91.204:5000",
+  baseURL: import.meta.env.VITE_BASE_URL,
   timeout: 3000,
   headers: {
     "Content-Type": "application/json;charset=UTF-8",
