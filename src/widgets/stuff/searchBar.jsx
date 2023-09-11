176b940e599bfe55df896562745c5b01171e3516
EuDs63 <erica23187@gmail.com>
2023-09-11 23:32:59 +0800

    样式实现

--- src/widgets/stuff/searchBar.jsx
+++ src/widgets/stuff/searchBar.jsx
@@ -1,7 +1,14 @@
-import 
-{
+import {
+    Collapse,
     Button,
     Input,
+    Card,
+    CardBody,
+    Typography,
+    Menu,
+    MenuHandler,
+    MenuList,
+    MenuItem,
 } from "@material-tailwind/react";
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
@@ -9,6 +16,7 @@ export function SearchBar() {
     const navigate = useNavigate();
     const [searchText, setSearchText] = useState("");
     const handleInputChange = (e) => {
+        setOpen(true);
         setSearchText(e.target.value);
     };
 
@@ -16,14 +24,26 @@ export function SearchBar() {
         navigate(`/book/search?query=${searchText}`);
     };
 
+    const [open, setOpen] = useState(false);
+
+    const toggleOpen = () => setOpen((cur) => !cur);
+
     return (
         <>
-            <div className="mr-auto md:mr-0 md:w-56">
-                <Input
-                    label="书名、作者"
-                    onChange={handleInputChange}
-                    value={searchText}
-                />
+            <div className="mr-auto md:mr-0 md:w-56 flex-col">
+
+                <Menu className="flex-grow">
+                    <MenuHandler>
+                        <Input
+                            label="书名、作者"
+                            onChange={handleInputChange}
+                            value={searchText}
+                        />
+                    </MenuHandler>
+                    <MenuList className="max-h-36">
+                        <MenuItem>Menu Item 1</MenuItem>
+                    </MenuList>
+                </Menu>
             </div>
             <Button
                 variant="filled"
@@ -33,6 +53,7 @@ export function SearchBar() {
             >
                 搜索
             </Button>
+
         </>
     );
 
